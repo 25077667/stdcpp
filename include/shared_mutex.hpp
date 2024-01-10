53f88676a36d1d1aa652526004a4a1a39423eb9d
scc <scc@teamt5.org>
2024-01-10 16:12:22 +0800

    feat: add shared_mutex

--- include/shared_mutex.hpp
+++ include/shared_mutex.hpp
@@ -2,4 +2,101 @@
 #define __SCC_STDCPP_SHARED_MUTEX_HPP__
 #pragma once
 
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+
+namespace stdcpp
+{
+    namespace v1
+    {
+        class shared_mutex
+        {
+        public:
+            shared_mutex() = default;
+            ~shared_mutex() = default;
+
+            shared_mutex(const shared_mutex &) = delete;
+            shared_mutex &operator=(const shared_mutex &) = delete;
+
+            // Exclusive locking
+            void lock()
+            {
+                std::unique_lock<std::mutex> lk(mtx);
+                exclusive_wait.wait(lk, [this]
+                                    { return shared_count == 0 && !exclusive; });
+                exclusive = true;
+            }
+
+            bool try_lock()
+            {
+                std::lock_guard<std::mutex> lk(mtx);
+                if (shared_count == 0 && !exclusive)
+                {
+                    exclusive = true;
+                    return true;
+                }
+                return false;
+            }
+
+            void unlock()
+            {
+                {
+                    std::lock_guard<std::mutex> lk(mtx);
+                    exclusive = false;
+                }
+                cv.notify_all();
+            }
+
+            // Shared locking
+            void lock_shared()
+            {
+                std::unique_lock<std::mutex> lk(mtx);
+                cv.wait(lk, [this]
+                        { return !exclusive; });
+                ++shared_count;
+            }
+
+            bool try_lock_shared()
+            {
+                std::lock_guard<std::mutex> lk(mtx);
+                if (!exclusive)
+                {
+                    ++shared_count;
+                    return true;
+                }
+                return false;
+            }
+
+            void unlock_shared()
+            {
+                {
+                    std::lock_guard<std::mutex> lk(mtx);
+                    --shared_count;
+                    if (shared_count == 0)
+                    {
+                        exclusive_wait.notify_one();
+                    }
+                }
+                if (shared_count == 0)
+                {
+                    cv.notify_one();
+                }
+            }
+
+            // Native handle (implementation-defined)
+            // This example does not implement a native_handle method.
+
+        private:
+            std::mutex mtx;
+            std::condition_variable cv;
+            std::condition_variable exclusive_wait;
+            int shared_count = 0;
+            bool exclusive = false;
+        };
+    }
+
+    using shared_mutex = v1::shared_mutex;
+}
+
 #endif // __SCC_STDCPP_SHARED_MUTEX_HPP__
--- test/shared_mutex.cpp
+++ test/shared_mutex.cpp
@@ -1,7 +1,38 @@
 #include <shared_mutex.hpp>
 #include <gtest/gtest.h>
 
-TEST(shared_mutex, shared_mutex)
+TEST(stdcpp_shared_mutex, lockShard_duplicate_lock)
 {
-    ASSERT_TRUE(true);
+    stdcpp::shared_mutex mtx;
+    mtx.lock_shared();
+    ASSERT_TRUE(mtx.try_lock_shared());
+    mtx.unlock_shared();
+    mtx.unlock_shared();
+}
+
+TEST(stdcpp_shared_mutex, blockExclusive_duplicate_lock)
+{
+    stdcpp::shared_mutex mtx;
+    mtx.lock();
+    ASSERT_FALSE(mtx.try_lock());
+    mtx.unlock();
+
+    mtx.lock_shared();
+    ASSERT_FALSE(mtx.try_lock());
+    mtx.unlock_shared();
+
+    ASSERT_TRUE(mtx.try_lock());
+    mtx.unlock();
+}
+
+TEST(stdcpp_shared_mutex, lockWithUniqueLock_LockingCS_ExpectNotReentrant)
+{
+    stdcpp::shared_mutex mtx;
+    std::unique_lock<stdcpp::shared_mutex> lk(mtx);
+    ASSERT_FALSE(mtx.try_lock());
+    ASSERT_FALSE(mtx.try_lock_shared());
+    lk.unlock();
+    ASSERT_TRUE(mtx.try_lock());
+    ASSERT_FALSE(mtx.try_lock_shared());
+    mtx.unlock();
 }
