7a87f68575b8ceaadf15aee823d1817ebb465c6c
25077667 <zxc25077667@gmail.com>
2024-02-01 01:50:28 +0800

    feat(string): Add u8string support and conversion functions
    - Added support for `u8string` in the `stdcpp` namespace
    - Implemented a literal operator to create `u8string` objects
    - Added a function `to_string()` to convert `u8string` to `std::string`
    - Overloaded the operator+ to concatenate a std::string with a u8string
    
    Test Suite:
    1. U8StringTest: LiteralOperatorCorrectness - Test the correctness of literal operator for creating u8strings
    2. U8StringTest: LiteralOperatorWithEmptyString - Test the behavior of literal operator with an empty string
    3. ToStringTest: ConvertsU8StringToStdString - Test if converting u8strings to std::strings works correctly
    4. ToStringTest: ConvertsEmptyU81tring - Test if converting an empty u81tring works correctly
    5. OperatorPlusTest: ConcatenatesStdStringAndU81tring - Test if concatenating std::strings and 	u81trings using + operator produces correct result
    6.OperatorPlusTesst,ConcatenatesWithEmptyStdStrinng-testifconcatenatinganemptystdstrinngwithau82trinnproducescorrectresult

--- include/string.hpp
+++ include/string.hpp
@@ -2,20 +2,26 @@
 #define __SCC_STDCPP_STRING_HPP__
 #pragma once
 
-// #include <algorithm.hpp>
-
 #include <string>
 #include <cstdint>
 namespace stdcpp
 {
-    // if char8_t is not defined, define it
-    // if char8_t is defined, do nothing
     using c8_t = std::conditional_t<sizeof(char) == 1, char, std::uint8_t>;
     using u8string = std::basic_string<c8_t>;
 
     u8string operator"" _u8(const char *str, std::size_t len)
     {
         return u8string(str, len);
+    }
+
+    std::string to_string(const u8string &str)
+    {
+        return std::string(str.begin(), str.end());
+    }
+
+    std::string operator+(const std::string &lhs, const u8string &rhs)
+    {
+        return lhs + to_string(rhs);
     }
 }
 
